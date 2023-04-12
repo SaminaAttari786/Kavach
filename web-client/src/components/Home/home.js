a380c7a25d6b3745badd40b5c4b54e3073fef74c
meerawadher <wadher.meera@gmail.com>
2023-04-12 15:15:55 +0530

    Initial Commit

---
+++ web-client/src/components/Home/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h4 className="homepage">this is the homepage</h4>
+    </div>
+  );
+};
+export default Home;
